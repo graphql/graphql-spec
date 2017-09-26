e5d9f9da88d393cf39650573ad2083b0f91e24df
Lee Byron <lee@leebyron.com>
2017-09-25 22:05:49 -0700

    Fix publish.sh

--- publish.sh
+++ publish.sh
@@ -43,7 +43,7 @@ git add -A .
 if git diff --staged --quiet; then
   echo "Nothing to publish"
 else
-  git diff --quiet git commit -a -m "Deploy to GitHub Pages"
+  git commit -a -m "Deploy to GitHub Pages"
   git push > /dev/null 2>&1
   echo "Pushed"
 fi
