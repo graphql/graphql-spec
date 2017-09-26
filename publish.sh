2266069610dfed4fb6d0ab858a1ada4c15279251
Lee Byron <lee@leebyron.com>
2017-09-25 19:16:25 -0700

    Fix remaining issues

--- publish.sh
+++ publish.sh
@@ -5,6 +5,9 @@
 echo "Building spec"
 npm run build > /dev/null 2>&1
 
+# Determine if this is a tagged release
+GITTAG=$(git tag --points-at HEAD)
+
 # Check out gh-pages locally.
 echo "Cloning gh-pages"
 rm -rf gh-pages
@@ -14,13 +17,12 @@ pushd gh-pages > /dev/null 2>&1
 # Replace /draft with this build.
 echo "Publishing to: /draft"
 rm -rf draft
-cp -r ../out draft
+cp -r ../out/ draft
 
 # If this is a tagged commit, publish to a permalink and index.
-GITTAG=$(git tag --points-at HEAD)
 if [ -n "$GITTAG" ]; then
   echo "Publishing to: /$GITTAG"
-  cp -r ../out "$GITTAG"
+  cp -r ../out/ "$GITTAG"
 
   echo "Linking from: / (index)"
   echo '<html>
