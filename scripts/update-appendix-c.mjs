02670fcb25d652b85317947118dc6b54862408a3
Martin Bonnin <martin@mbonnin.net>
2025-05-28 17:34:11 +0200

    Add appendix C - builtin definitions

---
+++ scripts/update-appendix-c.mjs
@@ -0,0 +1,22 @@
+import { writeFile } from 'node:fs/promises';
+import { printIntrospectionSchema, buildSchema } from 'graphql';
+
+const FILE = './spec/Appendix C -- Built-in Definitions.md';
+
+const sdl = printIntrospectionSchema(buildSchema(`type Query { i: Int }`));
+const prefix = `
+# C. Appendix: Type System Definitions
+
+This appendix lists all the type system definitions mentioned throughout this
+specification.
+
+The descriptions are non-normative. Implementations are recommended to use them
+for consistency but different descriptions are allowed.
+
+\`\`\`graphql
+`
+
+const suffix = `
+\`\`\`
+`
+await writeFile(FILE, prefix + sdl + suffix);
