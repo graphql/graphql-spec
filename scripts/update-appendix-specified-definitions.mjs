11c6664e2ea65eddd812bac73b9e7341b4610292
Lee Byron <lee@leebyron.com>
2025-09-01 01:49:17 -0700

    condense the spec defs appendix (#1186)
    * condense the spec defs appendix
    
    * fix js-ahead-of-spec

--- scripts/update-appendix-specified-definitions.mjs
+++ scripts/update-appendix-specified-definitions.mjs
@@ -1,28 +1,56 @@
-import { writeFile } from 'node:fs/promises';
-import { printIntrospectionSchema, buildSchema, specifiedScalarTypes, printType } from 'graphql';
+import prettier from "prettier";
+import { writeFile } from "node:fs/promises";
+import {
+  printIntrospectionSchema,
+  buildSchema,
+  specifiedScalarTypes,
+  printType,
+  parse,
+  print,
+  visit,
+  Kind
+} from "graphql";
 
-const FILE = './spec/Appendix D -- Specified Definitions.md';
-function printSpecifiedScalars() {
-  return specifiedScalarTypes
-    .map((type) => printType(type))
-    .join('\n\n');
+function stripDescriptions(sdl) {
+  const ast = parse(sdl);
+  const noDescAst = visit(ast, {
+    enter: (node) => {
+      // Not in spec yet
+      if (node.name?.value === "FRAGMENT_VARIABLE_DEFINITION") {
+        return null
+      }
+    },
+    leave: (node) => ({ ...node, description: undefined })
+  });
+  return print(noDescAst);
 }
 
-const introspectionSchema = printIntrospectionSchema(buildSchema(`type Query { i: Int }`));
-const prefix = `
-# C. Appendix: Type System Definitions
+function printSpecifiedScalars() {
+  return specifiedScalarTypes
+    .map((type) => stripDescriptions(printType(type)))
+    .join("\n\n");
+}
 
-This appendix lists the specified type system definitions.
+const introspectionSchema = stripDescriptions(
+  printIntrospectionSchema(buildSchema(`type Query { i: Int }`))
+);
 
-The descriptions are non-normative. Implementations are recommended to use them
-for consistency but different descriptions are allowed.
+const allSpecifiedTypesSDL = prettier
+  .format(printSpecifiedScalars() + "\n\n" + introspectionSchema, {
+    parser: "graphql"
+  })
+  .trimEnd();
+
+await writeFile(
+  "./spec/Appendix D -- Specified Definitions.md",
+  `# D. Appendix: Type System Definitions
+
+This appendix lists all type system definitions specified in this document.
 
 The order of types, fields, arguments, values and directives is non-normative.
 
 \`\`\`graphql
-`
-
-const suffix = `
+${allSpecifiedTypesSDL}
 \`\`\`
 `
-await writeFile(FILE, prefix + printSpecifiedScalars() + '\n\n' + introspectionSchema + suffix);
+);
