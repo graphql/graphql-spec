e8c59a2da9f94b597e5aa39849fdbb158f56dd8b
Benjie Gillam <benjie@jemjie.com>
2026-09-05 15:09:10 +0100

    Missed a file

---
+++ wg.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/** @type {import('wgutils').Config} */
+const config = {
+  name: "GraphQL Specification",
+  repoUrl: "https://github.com/graphql/graphql-spec",
+  meetings: false,
+  spec: {
+    sentenceName: "the GraphQL specification",
+    url: "https://spec.graphql.org"
+  }
+};
+
+module.exports = config;
